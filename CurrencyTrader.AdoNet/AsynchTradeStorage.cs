6b33f49c685fcb7366554cce729a2e890b7a02be
Tracy Yang <jewel7meme@gmail.com>
2016-11-10 11:39:32 -0600

    Changed namespace and created AsynchTradeStorage

---
+++ CurrencyTrader.AdoNet/AsynchTradeStorage.cs
@@ -0,0 +1,12 @@
+﻿using CurrencyTrader.Contracts;
+
+namespace CurrencyTrader.AdoNet
+{
+    class AsynchTradeStorage : ITradeStorage
+    {
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+
+        }
+    }
+}
