efb84307c596b05f1d3c0ce6bcdfde61f0e1f763
jewel7meme <jewel7meme@gmail.com>
2016-11-21 16:57:38 -0600

    Add an Asynchronous decorator to the AdoNetTradeStorage class so that updating the database is done in a separate task.

--- CurrencyTrader.AdoNet/AsynchTradeStorage.cs
+++ CurrencyTrader.AdoNet/AsynchTradeStorage.cs
@@ -1,12 +1,16 @@
 ﻿using CurrencyTrader.Contracts;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CurrencyTrader.AdoNet
 {
     class AsynchTradeStorage : ITradeStorage
     {
+        protected AsynchTradeStorage SynchTradeStorage;
         public void Persist(IEnumerable<TradeRecord> trades)
         {
-
+            Task.Run(() => SynchTradeStorage.Persist(trades));
+            
         }
     }
 }
